b6daf228a7ad14b66e800074baf531bead3d5689
Luizhnrs <Luizhnrsg@gmail.com>
2025-10-07 13:18:53 -0300

    FEAT: Models

---
+++ src/models/feedback.ts
@@ -0,0 +1,7 @@
+export interface Feedback {
+  id: string;        
+  name?: string;       
+  message: string;     
+  rating?: number;     
+  createdAt: string;    
+}
---
+++ src/models/request.ts
@@ -0,0 +1,5 @@
+export interface CreateFeedbackRequest {
+  name?: string;
+  message: string;
+  rating?: number;
+}
